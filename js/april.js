a76d36666c93b434a56167dd0da930b3d7e93137
Petr Šťastný <petr.stastny01@gmail.com>
2020-01-31 11:29:51 +0100

    Added april CSS

---
+++ js/april.js
@@ -0,0 +1,28 @@
+let d = new Date();
+if(d.getMonth() != 3 || d.getDate() != 1)
+{
+    console.log("Here goes nothing");
+}
+else
+{
+    executeApril();
+}
+
+function executeApril() {
+    console.log("It's party time!!");
+
+    document.write('<link rel="stylesheet" type="text/css" href="css/april.css">');
+
+    sayori(0.05);
+}
+
+function sayori(chance) {
+    console.log("JUST MONIKA");
+    if(Math.random() <= chance) {
+        console.log("Sayori STRIKES");
+        document.body.innerHTML = '<img class="disappearing" src="img/screenshot0060.png" />' + document.body.innerHTML;
+
+        if(chance < 1.5)
+            window.setTimeout(() => sayori(chance + 0.2), 2000);
+    }
+}
