5533ad1753063fd9fcbc3326127724e72f5d871e
Petr Šťastný <petr.stastny01@gmail.com>
2020-03-01 15:44:42 +0100

    Added and enabled karel april

--- js/april.js
+++ js/april.js
@@ -1,16 +1,16 @@
 /* I said there is nothing to see here! */
 
 let d = new Date();
-if(d.getMonth() != 3 || d.getDate() != 1)
+if((d.getMonth() != 3 && false) || (d.getDate() != 1 && false))
 {
     console.log("Here comes nothing");
 }
 else
 {
-    executeApril();
+    executeKarelApril();
 }
 
-function executeApril() {
+function executeDDLCApril() {
     console.log("It's party time!!");
 
     document.body.innerHTML += '<link rel="stylesheet" type="text/css" href="css/april.css">';
@@ -27,4 +27,34 @@ function sayori(chance) {
         if(chance < 1.5)
             window.setTimeout(() => sayori(chance + 0.2), 2000);
     }
+}
+
+function executeKarelApril() {
+    let orgs = document.getElementsByClassName("org-name");
+    for (let i = 0; i < orgs.length; i++) {
+        const org = orgs[i];
+        org.innerText = "Karel Sedláček";
+    }
+
+    window.setTimeout(() => replaceRandomWordWithKarel(), 2000);
+}
+
+const regex_word = /[a-zA-ZěščřžýáíéůúĚŠČŘŽÝÁÍÉŮÚ]{2,}/g
+function replaceRandomWordWithKarel() {
+    let all_paragraphs = document.getElementsByTagName("p");
+    let random_paragraph = all_paragraphs[Math.floor((Math.random() * (all_paragraphs.length - 1)) + 0)];
+    let words = random_paragraph.innerText.match(regex_word);
+    if(words != null){
+        let words_without_karel = [];
+        for (let i = 0; i < words.length; i++){
+            if(words[i] != "Karel")
+            {
+                words_without_karel.push(words[i]);
+            }
+        }
+        let random_word = words_without_karel[Math.floor((Math.random() * (words_without_karel.length - 1)) + 0)];
+        random_paragraph.innerText = random_paragraph.innerText.replace(random_word, "Karel");
+    }
+
+    window.setTimeout(() => replaceRandomWordWithKarel(), 500);
 }
