fa52836035b4cae956503d2c8f733034cc3c9f33
Petr Šťastný <petr.stastny01@gmail.com>
2020-03-08 17:22:30 +0100

    Renamed harmonogram to schedule

---
+++ js/schedule.js
@@ -0,0 +1,18 @@
+/* This file provides various helper functions that help
+ * with lectures. This parses /schedule.json in order
+ * to provide useful information.
+ */
+
+ schedule = null;
+
+ /* Load schedule from JSON file and store it into global variable */
+ function loadSchedule() {
+    var req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open('GET', "https://prenoc.cz/schedule.json", true);
+    req.onload  = function() {
+       var jsonResponse = req.response;
+       schedule = JSON.parse(jsonResponse);
+    };
+    req.send(null);
+ }
