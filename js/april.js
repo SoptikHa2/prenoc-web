36f4b010284a10dd00078b7019740e128089bf0b
Petr Šťastný <petr.stastny01@gmail.com>
2020-03-18 20:13:56 +0100

    Changed april chance to 20%, it can be triggered between 1 and 3AM
    - Changed april chance (that it will fire up) from 0.1 to 0.2
    - April event now can be executed either anytime on 1st April,
      or between 1 and 3 AM. On 1st April, between 1 and 3 AM, the
      chance for it to execute is increased (currently to 0.36)
    - Changed april event condition to be described positively
      (if this passes, event will start) instead of negatively

--- js/april.js
+++ js/april.js
@@ -1,15 +1,20 @@
 /* I said there is nothing to see here! */
 
-let april_chance = 0.1;
+let april_chance = 0.2;
 
 let d = new Date();
-if((d.getMonth() != 3) || (d.getDate() != 1) || Math.random() > april_chance)
+if(
+    // It's 1st april and april chance is satisfied
+    ((d.getMonth() == 3) && (d.getDate() == 1) && Math.random() < april_chance) ||
+    // It's between 1 and 3 am and chance is satisfied
+    ((d.getHours() >= 1 && d.getHours() <= 3) && Math.random() < april_chance)
+)
 {
-    console.log("Here comes nothing");
+    executeKarelApril();
 }
 else
 {
-    executeKarelApril();
+    console.log("Here comes nothing");
 }
 
 function executeDDLCApril() {
