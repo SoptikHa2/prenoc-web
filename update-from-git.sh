0f18c67218deaa5a6507faa4505f255f342b79e1
Petr Šťastný <petr@soptik.tech>
2020-08-21 07:12:02 +0200

    Changed script to load beta branch

--- update-from-git.sh
+++ update-from-git.sh
@@ -3,7 +3,7 @@
 cd /var/www/prenoc || return
 # Force pull
 git fetch --all
-git reset --hard origin/master
+git reset --hard origin/beta
 # Write last commit hash to version.txt
 echo $(git log -1 --oneline | cut -d' ' -f1) > version.txt
 )
